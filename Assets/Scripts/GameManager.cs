364dd4b79914f151b53a9c220c4e0255d4364a71
Abner <abner.matadamas@uteca.edu.mx>
2025-12-03 19:57:13 -0600

    Menu y Mapa
    Mapa de Niveles
    Selector de Niveles
    Musica del Menu
    Musica del Juego
    Efectos de sonido
    3 niveles elegibles

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,15 +8,17 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private EnemyManager enemyManager;
     [SerializeField]
+    private PlantManager plantManager;
+    [SerializeField]
     private UnityEvent onWinGame;
     [SerializeField]
     private UnityEvent onLoseGame;
-    
     public void StartLevel()
     {
         LevelData currentLevel = gameData.levels[gameData.currentLevelIndex];
         enemyManager.SetEnemiesToSpawn(currentLevel.enemiesToSpawn);
         enemyManager.StartSpawningEnemies();
+        plantManager.SetAvailablePlants(currentLevel.availablePlants);
     }
     public void WinGame()
     {
